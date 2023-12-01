5ff92088f768999c4bbc79da9b862488de885eb1
eddie-dotcom <edmondpaakumahjnr@gmail.com>
2023-12-01 16:45:03 +0000

    next step

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
+//Hello World in Java
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-        
+        System.out.println("Hello World!");
+
     }
 }
---
+++ WelcomeWithThreeMessages.java
@@ -0,0 +1,10 @@
+public class WelcomeWithThreeMessages {
+    
+        public static void main(String[] args) {
+            
+            System.out.println("Programming is great fun!");
+            System.out.println("Fundamentals First");
+            System.out.println("Problem Driven");
+        }
+    }
+
